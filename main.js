3262fefb70634366ebe8f4663b1103e4e621d2e8
gbachik <gbachik@gmail.com>
2014-06-23 10:48:17 -0600

    Hello World Box

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-alert('Hello Boulder!');
+
+function alertName() {
+	var yourName = prompt('Whats your name?');
+	if(yourName.length === 0){
+		alert('YOU MUST ENTER SOMETHING!')
+		console.log('Fail!');
+		alertName();
+	}else{
+		alert('Hello ' + yourName);
+		console.log('Successfull!');
+	}
+}
+
+alertName();
